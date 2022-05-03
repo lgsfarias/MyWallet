605832ba593b6be4f7dc1df7fb136e06bc1bef5a
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-05-03 16:44:22 -0300

    SignUp screen layout

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,132 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import axios from 'axios';
 
 const SignUp = () => {
-    return <h1>SignUp</h1>;
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post('http://localhost:5000/signup', {
+                name,
+                email,
+                password,
+                confirmPassword,
+            })
+            .then((res) => {
+                navigate('/');
+            })
+            .catch((err) => {
+                console.log(err.response.data);
+            });
+    };
+
+    return (
+        <SignUpContainer>
+            <h1>MyWallet</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Nome"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    type="email"
+                    placeholder="E-mail"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Senha"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Confirme a senha"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <button type="submit">Cadastrar</button>
+                <h2 className="link" onClick={() => navigate('/')}>
+                    Já tem uma conta? Entre agora!
+                </h2>
+            </form>
+        </SignUpContainer>
+    );
 };
 
 export default SignUp;
+
+const SignUpContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background-color: #8c11be;
+
+    h1 {
+        font-family: 'Saira Stencil One', cursive;
+        font-size: 32px;
+        line-height: 50px;
+        color: #fff;
+        font-weight: bold;
+        margin-bottom: 24px;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        max-width: 400px;
+        padding: 0 24px;
+
+        input {
+            width: 100%;
+            height: 58px;
+            border: none;
+            border-radius: 5px;
+            padding: 0 16px;
+            margin-bottom: 13px;
+            font-size: 20px;
+            line-height: 23px;
+            color: #000000;
+        }
+
+        button {
+            width: 100%;
+            height: 46px;
+            border: none;
+            border-radius: 5px;
+            background-color: #a328d6;
+            color: #fff;
+            font-size: 20px;
+            font-weight: bold;
+            cursor: pointer;
+            margin-bottom: 36px;
+
+            &:hover,
+            &:focus {
+                opacity: 0.9;
+            }
+        }
+
+        .link {
+            font-size: 15px;
+            font-weight: bold;
+            color: #fff;
+            cursor: pointer;
+        }
+    }
+`;
